35b1eab285bd9eb47836b58e6c7718e27c463577
Pramit Bhatia <pramitbhatia25@gmail.com>
2022-09-13 20:02:49 -0400

    Edits

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
   <div>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/Portfolio" element={<Layout />}>
         <Route index element= {<Home/>} />
       </Route>
     </Routes>
