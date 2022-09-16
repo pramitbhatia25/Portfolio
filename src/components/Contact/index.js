e2b40b2ccbc87c0fc066c6f14711fc4ba67418b1
Pramit Bhatia <pramitbhatia25@gmail.com>
2022-09-16 18:11:35 -0400

    Edits

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,12 @@
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import {useState, useEffect} from "react";
+import { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-
+    const refForm = useRef();
     useEffect(() => {
 
         let timeoutId = setTimeout(() => {
@@ -17,41 +18,63 @@ const Contact = () => {
         }
     }, [])
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_o1xbn8k',
+            'template_bvcoawo',
+            refForm.current,
+            'RVo9CxTqD3INEUHwE'
+        )
+            .then(
+                () => {
+                    alert("Message successfully sent!");
+                    window.location.reload(false);
+                },
+                () => {
+                    alert("Failed to send the message, try again.");
+                }
+            )
+            ;
+    }
     return <div>
         <div className="container contact-page">
             <div className="text-zone">
                 <h1>
                     <AnimatedLetters strArray={["C", "o", "n", "t", "a", "c", "t", " ", "m", "e"]}
-                    idx = {15}
-                    letterClass = {letterClass} />
+                        idx={15}
+                        letterClass={letterClass} />
                 </h1>
                 <p>
-                    Lorem Ipsum 
+                Hey! If you have any opportunities or would just like to chat with me, send me a message! I promise I'll get back ASAP :)
+                <br />
+                PS: Yes, the website works, I will get the message.
                 </p>
                 <div className="contact-form">
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className="half">
-                                <input type="text" name="name" placeholder="Name" required/>
+                                <input type="text" name="name" placeholder="Name" required />
                             </li>
                             <li className="half">
-                                <input type="email" name="email" placeholder="Email" required/>
+                                <input type="email" name="email" placeholder="Email" required />
                             </li>
                             <li className="full">
-                                <input type="text" name="subject" placeholder="Subject" required/>
+                                <input type="text" name="subject" placeholder="Subject" required />
                             </li>
                             <li className="full">
-                                <textarea name="message" placeholder="Message" required/>
+                                <textarea name="message" placeholder="Message" required />
                             </li>
                             <li className="full">
-                                <input type="submit" value="SEND" className="flat-button"/>
+                                <input type="submit" value="SEND" className="flat-button" />
                             </li>
                         </ul>
                     </form>
                 </div>
             </div>
         </div>
-        <Loader type="pacman"/>
+        <Loader type="pacman" />
     </div>
 }
 
